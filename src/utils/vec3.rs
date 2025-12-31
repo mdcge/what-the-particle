611c4ca51ab6ec2fe116086546f6056b494befd9
Max Generowicz <max@generowicz.net>
2025-12-31 19:48:47 +0000

    Overload * for Vec3 (with test)

--- src/utils/vec3.rs
+++ src/utils/vec3.rs
@@ -29,6 +29,23 @@ impl Sub<Vec3> for Vec3 {
     }
 }
 
+// Multiplication (*)
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Vec3 {
+        Vec3(self.0*rhs, self.1*rhs, self.2*rhs)
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3(self*rhs.0, self*rhs.1, self*rhs.2)
+    }
+}
+
 // Comparison (==)
 impl PartialEq<Vec3> for Vec3 {
     fn eq(&self, rhs: &Vec3) -> bool {
@@ -76,5 +93,25 @@ mod tests {
         assert_vec3_eq!(v1 - v3, Vec3(-3.7, 0.8, 1.5));
         assert_vec3_eq!(v2 - v3, Vec3(-2.7, 2.8, 4.5));
     }
+
+    #[test]
+    fn test_mul() {
+        let v1 = Vec3(0.0, 1.0, 2.0);
+        let v2 = Vec3(5.2, 3.1, 4.7);
+        let v3 = Vec3(-0.9, 7.2, -2.3);
+        let f1 = 4.3;
+        let f2 = -1.2;
+        let f3 = 0.0;
+        assert_vec3_eq!(v1 * f1, Vec3(0.0, 4.3, 8.6));
+        assert_vec3_eq!(v3 * f2, Vec3(1.08, -8.64, 2.76));
+        assert_vec3_eq!(v1 * f1, f1 * v1);
+        assert_vec3_eq!(v1 * f2, f2 * v1);
+        assert_vec3_eq!(v1 * f3, f3 * v1);
+        assert_vec3_eq!(v2 * f1, f1 * v2);
+        assert_vec3_eq!(v2 * f2, f2 * v2);
+        assert_vec3_eq!(v2 * f3, f3 * v2);
+        assert_vec3_eq!(v3 * f1, f1 * v3);
+        assert_vec3_eq!(v3 * f2, f2 * v3);
+        assert_vec3_eq!(v3 * f3, f3 * v3);
     }
 }
