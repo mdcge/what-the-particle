e0c150f56a0f7fa4aa2497ca19212aed3b355e3f
Max Generowicz <max@generowicz.net>
2026-01-01 18:49:36 +0000

    Add WASMWorld struct with basic constructor

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,20 @@ pub mod utils;
 pub mod particle;
 pub mod geometry;
 pub mod sim;
+
+use wasm_bindgen::prelude::*;
+use crate::sim::world::World;
+use crate::geometry::volume::Volume;
+#[wasm_bindgen]
+pub struct WASMWorld {
+    world: World,
+}
+
+#[wasm_bindgen]
+impl WASMWorld {
+    #[wasm_bindgen(constructor)]
+    pub fn new(volume_size: f64, dt: f64) -> Self {
+        let volume = Volume::new(volume_size);
+        WASMWorld { world: World::new(vec![], volume, dt) }
+    }
+}
