6e37612c5bd375e6c03039e9843614a91e3226fc
Max Generowicz <max@generowicz.net>
2026-01-01 18:38:02 +0000

    Prepare WASM interface

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use rust::sim::world::World;
-use rust::particle::particle::{Particle, ParticleType};
-use rust::geometry::volume::Volume;
-use rust::utils::vec3::Vec3;
-
 fn main() {
-    let volume = Volume::new(10.0);
-    let electron = Particle::new(Vec3(0.0, 0.0, 0.0), Vec3(1.0, 0.0, 0.0), ParticleType::Electron);
-    let mut world = World::new(vec![electron], volume, 0.001);
-    while world.has_alive_particles() {
-        world.step();
-    }
+    println!("Hello World!");
 }
