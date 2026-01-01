7f6b5aaa9d9f99a844664367210a610ab1fe665c
Max Generowicz <max@generowicz.net>
2026-01-01 18:00:12 +0000

    Check if world has alive particles (with test)

--- src/sim/world.rs
+++ src/sim/world.rs
@@ -12,6 +12,10 @@ impl World {
     pub fn new(particle_list: Vec<Particle>, vol: Volume, timestep: f64) -> Self {
         World { time: 0.0, dt: timestep, particles: particle_list, volume: vol }
     }
+
+    pub fn has_alive_particles(&self) -> bool {
+        self.particles.iter().any(|p| p.state.alive)
+    }
 }
 
 
@@ -37,5 +41,22 @@ mod tests{
         assert_eq!(w2.particles.len(), 2);
         assert_eq!(w3.particles.len(), 1);
         assert_eq!(w4.particles.len(), 0);
+    }
+
+    #[test]
+    fn test_world_has_alive_particles() {
+        let v1 = Volume::new(10.0);
+        let v2 = Volume::new(28.4);
+        let p1 = Particle::new(Vec3(1.0, 2.0, -3.0), Vec3(5.0, 0.0, 0.0), ParticleType::Electron);
+        let p2 = Particle::new(Vec3(4.2, -1.5, 5.1), Vec3(3.4, -2.0, 0.7), ParticleType::Muon);
+        let p3 = Particle::new(Vec3(20.1, -10.3, -9.7), Vec3(-100.0, 0.0, -52.1), ParticleType::Gamma);
+        let w1 = World::new(vec![p1, p2.clone(), p3], v1.clone(), 0.1);
+        let w2 = World::new(vec![], v2.clone(), 0.01);
+        let w3 = World::new(vec![p2], v2, 0.005);
+        let w4 = World::new(vec![], v1, 1.0);
+        assert!(w1.has_alive_particles());
+        assert!(!w2.has_alive_particles());
+        assert!(w3.has_alive_particles());
+        assert!(!w4.has_alive_particles());
     }
 }
