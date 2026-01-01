e7bd8b96423dba2342eebad3a43d45d518f6658a
Max Generowicz <max@generowicz.net>
2026-01-01 18:49:58 +0000

    Add particle to WASMWorld

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ pub mod sim;
 use wasm_bindgen::prelude::*;
 use crate::sim::world::World;
 use crate::geometry::volume::Volume;
+use crate::particle::particle::{Particle, ParticleType};
+use crate::utils::vec3::Vec3;
+
 #[wasm_bindgen]
 pub struct WASMWorld {
     world: World,
@@ -17,5 +20,16 @@ impl WASMWorld {
     pub fn new(volume_size: f64, dt: f64) -> Self {
         let volume = Volume::new(volume_size);
         WASMWorld { world: World::new(vec![], volume, dt) }
+    }
+
+    // Add particle to simulation: API will need to be reviewed
+    pub fn add_particle(&mut self, name: &str, x: f64, y: f64, z: f64, px: f64, py: f64, pz: f64) {
+        let particle = match name {
+            "e-"    => Particle::new(Vec3(x, y, z), Vec3(px, py, pz), ParticleType::Electron),
+            "mu-"   => Particle::new(Vec3(x, y, z), Vec3(px, py, pz), ParticleType::Muon),
+            "gamma" => Particle::new(Vec3(x, y, z), Vec3(px, py, pz), ParticleType::Gamma),
+            _       => panic!("Unknown particle type."),
+        };
+        self.world.particles.push(particle);
     }
 }
