ddf3d436c7a971ad4b8d00e7e083640b77513a1a
Max Generowicz <max@generowicz.net>
2026-01-02 11:23:30 +0000

    Add ThreeJS to JS script and render correctly

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js';
+import Stats from 'https://cdnjs.cloudflare.com/ajax/libs/stats.js/17/Stats.js';
+
 import init, { WASMWorld } from "../pkg/mount_charles.js";
+
+// === Program stats (FPS, memory) ===
+const stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: memory
+document.body.appendChild(stats.dom);
+
+// === Basic scene setup ===
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x111111);
+
+// === Renderer ===
+const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('scene') });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// === Perspective camera ===
+const persp_camera = new THREE.PerspectiveCamera(
+    40, // field of view
+    window.innerWidth / window.innerHeight, // aspect ratio
+    0.1, // near clipping plane
+    10000 // far clipping plane
+);
+persp_camera.position.set(400, 400, 1100);
+
+renderer.render(scene, persp_camera);
 
 async function run() {
     await init(); // load WASM module
