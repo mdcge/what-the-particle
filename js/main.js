07397c4f339e8c44889da836709739f8d0d8986e
Max Generowicz <max@generowicz.net>
2026-01-02 11:46:05 +0000

    Move scene setup and rendering to Visualizer class

--- js/main.js
+++ js/main.js
@@ -4,18 +4,12 @@ import Stats from 'https://cdnjs.cloudflare.com/ajax/libs/stats.js/17/Stats.js';
 
 import init, { WASMWorld } from "../pkg/mount_charles.js";
 
+import Visualizer from './visualizer.js';
+
 // === Program stats (FPS, memory) ===
 const stats = new Stats();
 stats.showPanel(0); // 0: fps, 1: ms, 2: memory
 document.body.appendChild(stats.dom);
-
-// === Basic scene setup ===
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x111111);
-
-// === Renderer ===
-const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('scene') });
-renderer.setSize(window.innerWidth, window.innerHeight);
 
 // === Perspective camera ===
 const persp_camera = new THREE.PerspectiveCamera(
@@ -26,42 +20,21 @@ const persp_camera = new THREE.PerspectiveCamera(
 );
 persp_camera.position.set(400, 400, 1100);
 
-// === OrbitControls ===
-let controls = new OrbitControls(persp_camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.05;
-controls.enablePan = true;
-controls.enableZoom = true;
-controls.rotateSpeed = 0.8;
+// === Visualizer ===
+const visualizer = new Visualizer(persp_camera);
+visualizer.add_volume(500);
 
 // === Set camera ===
 let active_camera = persp_camera;
-controls.object = active_camera;
-
-// === Detector volume ===
-const size = 500;
-const geometry = new THREE.BoxGeometry(size, size, size);
-const edges = new THREE.EdgesGeometry(geometry);
-const edge_material = new THREE.LineBasicMaterial({ color: 0x555555 });
-const box_edges = new THREE.LineSegments(edges, edge_material);
-scene.add(box_edges);
-
-const volume_material = new THREE.MeshBasicMaterial({
-    color: 0x222222,
-    transparent: true,
-    opacity: 0.2,
-    side: THREE.BackSide,
-});
-const box_volume = new THREE.Mesh(geometry, volume_material);
-scene.add(box_volume);
+visualizer.controls.object = active_camera;
 
 function animate() {
     stats.begin();
 
     requestAnimationFrame(animate);
 
-    controls.update();
-    renderer.render(scene, active_camera);
+    visualizer.controls.update();
+    visualizer.renderer.render(visualizer.scene, active_camera);
 
     stats.end();
 }
---
+++ js/visualizer.js
@@ -0,0 +1,42 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js';
+
+export default class Visualizer {
+
+    constructor(camera) {
+        // === Scene ===
+        const scene = new THREE.Scene();
+        scene.background = new THREE.Color(0x111111);
+        // === Renderer ===
+        const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('scene') });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        // === OrbitControls ===
+        let controls = new OrbitControls(camera, renderer.domElement);
+        controls.enableDamping = true;
+        controls.dampingFactor = 0.05;
+        controls.enablePan = true;
+        controls.enableZoom = true;
+        controls.rotateSpeed = 0.8;
+
+        this.scene = scene;
+        this.renderer = renderer;
+        this.controls = controls;
+    }
+
+    add_volume(size, opacity=0.2) {
+        const geometry = new THREE.BoxGeometry(size, size, size);
+        const edges = new THREE.EdgesGeometry(geometry);
+        const edge_material = new THREE.LineBasicMaterial({ color: 0x555555 });
+        const box_edges = new THREE.LineSegments(edges, edge_material);
+        this.scene.add(box_edges);
+
+        const volume_material = new THREE.MeshBasicMaterial({
+            color: 0x222222,
+            transparent: true,
+            opacity: opacity,
+            side: THREE.BackSide,
+        });
+        const box_volume = new THREE.Mesh(geometry, volume_material);
+        this.scene.add(box_volume);
+    }
+}
