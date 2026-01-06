8a1add12275e1fcc3d069a2981a9fcfdf3f2f7ae
Max Generowicz <max@generowicz.net>
2026-01-06 15:40:08 +0000

    Fix RNG interfacing with JS

--- src/lib.rs
+++ src/lib.rs
@@ -18,9 +18,9 @@ pub struct WASMWorld {
 #[wasm_bindgen]
 impl WASMWorld {
     #[wasm_bindgen(constructor)]
-    pub fn new(volume_size: f64, dt: f64, seed: u64) -> Self {
+    pub fn new(volume_size: f64, dt: f64, seed: u32) -> Self {
         let volume = Volume::new(volume_size);
-        WASMWorld { world: World::new(vec![], volume, dt, seed) }
+        WASMWorld { world: World::new(vec![], volume, dt, seed as u64) }
     }
 
     pub fn step(&mut self) {
